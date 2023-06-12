8a7604b5ccceb25bd23dae7af5c09f28c3750e00
Matthew <matthew.shafran@gmail.com>
2023-06-12 19:46:23 -0400

    Fixing name

--- core/network.tf
+++ core/network.tf
@@ -1,5 +1,5 @@
 resource "aws_organizations_account" "network_account" {
-  name  = "my_new_account"
+  name  = "networking"
   email = "matthew.shafran+awsnetworking@gmail.com"
   parent_id = aws_organizations_organizational_unit.shared.id
 }
