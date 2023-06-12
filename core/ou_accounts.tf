ef3e37c28fa42ebc7fbe7138dc82b5fc7fa60083
Matthew <matthew.shafran@gmail.com>
2023-06-12 19:40:38 -0400

    Adding missing data

--- core/ou_accounts.tf
+++ core/ou_accounts.tf
@@ -2,20 +2,20 @@ data "aws_organizations_organization" "mshafran_org" {}
 
 resource "aws_organizations_organizational_unit" "dev" {
   name      = "dev"
-  parent_id = aws_organizations_organization.mshafran_org.roots[0].id
+  parent_id = data.aws_organizations_organization.mshafran_org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "uat" {
   name      = "uat"
-  parent_id = aws_organizations_organization.mshafran_org.roots[0].id
+  parent_id = data.aws_organizations_organization.mshafran_org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "prd" {
   name      = "prd"
-  parent_id = aws_organizations_organization.mshafran_org.roots[0].id
+  parent_id = data.aws_organizations_organization.mshafran_org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "shared" {
   name      = "shared"
-  parent_id = aws_organizations_organization.mshafran_org.roots[0].id
+  parent_id = data.aws_organizations_organization.mshafran_org.roots[0].id
 }
