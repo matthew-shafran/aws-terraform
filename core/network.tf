e247b320a1658ab75e09d63fbc06de39ceb05c77
Matthew <matthew.shafran@gmail.com>
2023-06-12 19:51:15 -0400

    Updating name

--- core/network.tf
+++ core/network.tf
@@ -1,4 +1,4 @@
-resource "aws_organizations_account" "network_account" {
+resource "aws_organizations_account" "networking_account" {
   name  = "networking"
   email = "matthew.shafran+awsnetworking@gmail.com"
   parent_id = aws_organizations_organizational_unit.shared.id
