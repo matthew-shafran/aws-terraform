f0a0c838949f7ff7e6622255d4eafc0d9ae70fbd
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-12 11:50:57 -0400

    Adding git ignore

--- auto-cloud/main.tf
+++ auto-cloud/main.tf
@@ -1,6 +1,5 @@
 module "autocloud-access-role" {
   source  = "autoclouddev/autocloud-access-role/aws"
   version = "1.1.3"
-  #autocloud_organization_id = ""
-
+  autocloud_organization_id = var.autocloud_organization_id
 }
