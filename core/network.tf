4414561380f97d9100de58a5c97cd20636eb90c0
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-13 19:45:07 -0400

    Adding a shared network

--- core/network.tf
+++ core/network.tf
@@ -37,6 +37,27 @@ module "dev-base-network" {
   ]
 }
 
+module "shared-base-network" {
+  source  = "cn-terraform/networking/aws"
+  version = "2.0.16"
+  name_prefix    = "dev"
+  single_nat     = false
+  vpc_cidr_block = "10.50.0.0/16"
+
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+  public_subnets_cidrs_per_availability_zone = [
+    "10.50.0.0/19",
+    "10.50.32.0/19"
+  ]
+  private_subnets_cidrs_per_availability_zone = [
+    "10.50.128.0/19",
+    "10.50.160.0/19"
+  ]
+}
+
 module "dev-base-network" {
   source  = "cn-terraform/networking/aws"
   version = "2.0.16"
