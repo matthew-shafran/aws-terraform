4bcff962524b5385c94b7f09ac23eb446ecd9efd
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-13 20:16:14 -0400

    Removing public ips from all but shared

--- core/network.tf
+++ core/network.tf
@@ -27,7 +27,7 @@ module "shared-base-network" {
     "us-east-1b"
   ]
   public_subnets_cidrs_per_availability_zone = [
-    "10.50.10.0/30"
+    "10.50.0.0/30"
   ]
   private_subnets_cidrs_per_availability_zone = [
     "10.50.128.0/19",
@@ -49,9 +49,7 @@ module "dev-base-network" {
     "us-east-1a",
     "us-east-1b"
   ]
-  public_subnets_cidrs_per_availability_zone = [
-    "10.100.0.0/30"
-  ]
+
   private_subnets_cidrs_per_availability_zone = [
     "10.100.128.0/19",
     "10.100.160.0/19"
@@ -72,9 +70,7 @@ module "uat-base-network" {
     "us-east-1a",
     "us-east-1b"
   ]
-  public_subnets_cidrs_per_availability_zone = [
-    "10.150.0.0/30"
-  ]
+
   private_subnets_cidrs_per_availability_zone = [
     "10.150.128.0/19",
     "10.150.160.0/19"
@@ -96,9 +92,7 @@ module "prd-base-network" {
     "us-east-1a",
     "us-east-1b"
   ]
-  public_subnets_cidrs_per_availability_zone = [
-    "10.200.0.0/30"
-  ]
+
   private_subnets_cidrs_per_availability_zone = [
     "10.200.128.0/19",
     "10.200.160.0/19"
