86a601de96aef775cb0d07f9b5b0fb9fb45c37f4
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-15 16:36:58 -0400

    Adding close on deletion to test

--- testapp/account.tf
+++ testapp/account.tf
@@ -5,8 +5,14 @@ locals {
 resource "aws_organizations_account" "testapp_account" {
   name  = "${local.app_code}-${var.environment_code}"
   email = "matthew.shafran+aws+${local.app_code}+${var.environment_code}@gmail.com"
+  close_on_deletion = true
   parent_id = var.ou_id
   role_name = "OrganizationAccountAccessRole"
+}
+
+resource "time_sleep" "wait_project_init" {
+  create_duration = "60s"
+  depends_on = [ aws_organizations_account.testapp_account ]
 }
 
 provider "aws" {
