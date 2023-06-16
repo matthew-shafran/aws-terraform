cf53b562f6ec3160905db3277d8a549063944766
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-16 14:41:29 -0400

    Test demo

--- testapp/account.tf
+++ testapp/account.tf
@@ -28,3 +28,7 @@ provider "aws" {
 }
 
 # Add code to delete default vpc
+resource "time_sleep" "wait_project_init" {
+  create_duration = "60s"
+  depends_on = [ aws_organizations_account.test-app_account ]
+}
