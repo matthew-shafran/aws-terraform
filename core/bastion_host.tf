85a0f395ca416e3edcbec1fcc6c3a7212ca40f4f
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-19 13:54:43 -0400

    Removing module to try a more lightweight one

--- core/bastion_host.tf
+++ core/bastion_host.tf
@@ -13,22 +13,3 @@ resource "aws_secretsmanager_secret_version" "mshafran_private_key" {
     secret_string = var.mshafran_private_ssh_key
 }
 
-module "bastion" {
-    source  = "Guimove/bastion/aws"
-    version = "3.0.6"
-    providers = {
-        aws = aws.network_account
-    }
-
-    bucket_name = "mshafran-bastion-logs"
-    region = "us-east-1"
-    vpc_id = module.shared-base-network.vpc_id
-    bastion_host_key_pair = local.mshafran_secret_key_name
-    create_dns_record = "false"
-    bastion_iam_policy_name = "bastionBucketAccess"
-    create_elb = "false"
-    auto_scaling_group_subnets = module.shared-base-network.public_subnets_ids
-    tags = {
-        "name" = "bastion_host"
-    }
-    }
