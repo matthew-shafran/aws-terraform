0f4bc859d94f121be645ae1904bd8c270f36b557
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-19 15:26:00 -0400

    removing extra var

--- core/variables.tf
+++ core/variables.tf
@@ -2,9 +2,3 @@ variable "aws_standard_region" {
     type        = string
     description = "Default region for resources"
 }
-
-variable "mshafran_private_ssh_key" {
-    type        = string
-    description = "Private SSH Key for user: mshafran"
-    sensitive   = true
-}
