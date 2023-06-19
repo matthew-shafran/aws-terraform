dda0ebc07623e28a9ca793d24fc0e0975766b5ca
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-19 13:38:08 -0400

    Adding sercret key

--- core/bastion_host.tf
+++ core/bastion_host.tf
@@ -1,3 +1,18 @@
+locals {
+    mshafran_secret_key_name = "mshafran_bastion_host_key"
+}
+
+resource "aws_secretsmanager_secret" "mshafran_ssh_key" {
+    provider = aws.network_account
+    name = local.mshafran_secret_key_name
+}
+
+resource "aws_secretsmanager_secret_version" "mshafran_private_key" {
+    provider = aws.network_account
+    secret_id     = aws_secretsmanager_secret.mshafran_ssh_key.id
+    secret_string = var.mshafran_private_ssh_key
+}
+
 module "bastion" {
     source  = "Guimove/bastion/aws"
     version = "3.0.6"
@@ -8,7 +23,7 @@ module "bastion" {
     bucket_name = "mshafran-bastion-logs"
     region = "us-east-1"
     vpc_id = module.shared-base-network.vpc_id
-    bastion_host_key_pair = "bastion_host_key"
+    bastion_host_key_pair = local.mshafran_secret_key_name
     create_dns_record = "false"
     bastion_iam_policy_name = "bastionBucketAccess"
     create_elb = "false"
