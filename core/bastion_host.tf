b149c3c02e6c0e1a1c156a9c6ea560a9764fe2d9
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-19 13:01:41 -0400

    Testing if i can skip elb

--- core/bastion_host.tf
+++ core/bastion_host.tf
@@ -4,15 +4,14 @@ module "bastion" {
     providers = {
         aws = aws.network_account
     }
-    
+
     bucket_name = "mshafran_bastion_logs"
     region = "us-east-1"
     vpc_id = module.shared-base-network.vpc_id
-    is_lb_private = "false"
     bastion_host_key_pair = "bastion_host_key"
     create_dns_record = "false"
     bastion_iam_policy_name = "bastionBucketAccess"
-    elb_subnets = module.shared-base-network.public_subnets_ids
+    create_elb = "false"
     auto_scaling_group_subnets = module.shared-base-network.public_subnets_ids
     tags = {
         "name" = "bastion_host"
