db64c6946553a3a0b2b9c009d2545a56bd6d4f5f
Matthew Shafran <matthew.shafran@gmail.com>
2023-06-19 12:48:15 -0400

    Adding bastion host

---
+++ core/bastion_host.tf
@@ -0,0 +1,20 @@
+module "bastion" {
+    source  = "Guimove/bastion/aws"
+    version = "3.0.6"
+    providers = {
+        aws = aws.network_account
+    }
+    
+    bucket_name = "mshafran_bastion_logs"
+    region = "us-east-1"
+    vpc_id = module.shared-base-network.vpc_id
+    is_lb_private = "false"
+    bastion_host_key_pair = "bastion_host_key"
+    create_dns_record = "false"
+    bastion_iam_policy_name = "bastionBucketAccess"
+    elb_subnets = module.shared-base-network.public_subnets_ids
+    auto_scaling_group_subnets = module.shared-base-network.public_subnets_ids
+    tags = {
+        "name" = "bastion_host"
+    }
+    }
